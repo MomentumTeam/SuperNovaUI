79371b6745603d042276334e5b548fe6bc220411
DanielMelio <danielezra10@gmail.com>
2021-06-30 18:08:50 +0300

    added axios interceptors code for Liora :crown: :girl:

---
+++ src/config/axios.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+const axiosApiInstance = axios.create();
+
+// Add a request interceptor
+axiosApiInstance.interceptors.request.use(function (config) {
+
+    // Do something before request is sent
+    return {
+        ...config,
+        headers: {
+            'authorization': getCookie('ppp')
+        },
+    };
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
+
+// Add a response interceptor
+axiosApiInstance.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+}, function (error) {
+    const originalRequest = error.config;
+
+    if (error.response.status === 403 && !originalRequest.retry) {
+        originalRequest.retry = true;
+        window.location.href = "http://localhost:9000/auth/login";
+        return axiosApiInstance(originalRequest);
+    }
+
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+});
+
+export default axiosApiInstance;
