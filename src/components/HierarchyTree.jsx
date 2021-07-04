30747eaa5e3a907b668685ad77c4a82ae1a80e00
Barak Steindl <steindl.barak@gmail.com>
2021-07-04 14:10:41 +0300

    console log

--- src/components/HierarchyTree.jsx
+++ src/components/HierarchyTree.jsx
@@ -1,21 +1,8 @@
 import { OrganizationChart } from "primereact/organizationchart";
 import "../assets/css/local/components/chart.min.css";
-import { updateNode } from "../service/TreeService";
 
 const HierarchyTree = ({ data }) => (
-  <OrganizationChart
-    selectionMode="single"
-    onSelectionChange={(event) => {
-      console.log("onSelectionChange");
-    }}
-    onNodeUnselect={(event) => {
-      console.log("onNodeUnselect");
-    }}
-    onNodeSelect={(event) => {
-      console.log("onNodeSelect");
-    }}
-    value={data}
-  />
+  <OrganizationChart selectionMode="single" value={data} />
 );
 
 export default HierarchyTree;
