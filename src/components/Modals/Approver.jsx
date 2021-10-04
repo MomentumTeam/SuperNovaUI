a95d4a3cf1eec42f1f65b139c4c489400297d2aa
shalev61 <shalev061@gmail.com>
2021-10-04 17:09:27 +0300

    change role modal

--- src/components/Modals/Approver.jsx
+++ src/components/Modals/Approver.jsx
@@ -2,32 +2,56 @@ import React, { useState } from 'react';
 import { searchApproverByDisplayNameReq } from '../../service/ApproverService';
 import { AutoComplete } from 'primereact/autocomplete';
 
-const Approver = ({ setValue, name }) => {
+const Approver = ({ setValue, name, multiple }) => {
+  const [ApproverSuggestions, setApproverSuggestions] = useState([]);
+  const [selectedApprover, setSelectedApprover] = useState(null);
 
-    const [ApproverSuggestions, setApproverSuggestions] = useState([]);
-    const [selectedApprover, setSelectedApprover] = useState(null);
+  const searchApprover = async (event) => {
+    const result = await searchApproverByDisplayNameReq(
+      event.query,
+      'COMMANDER'
+    );
+    setApproverSuggestions(result.approvers);
+  };
 
-    const searchApprover = async (event) => {
-        const result = await searchApproverByDisplayNameReq(event.query)
-        setApproverSuggestions(result.approvers);
-    }
+  return (
+    <div className='p-field-item'>
+      <div className={multiple ? 'AutoCompleteWrap' : ''}>
+        <label htmlFor='2022'>
+          <span className='required-field'>*</span>גורם מאשר
+        </label>
+        <AutoComplete
+          id='2022'
+          multiple={multiple}
+          value={selectedApprover}
+          suggestions={ApproverSuggestions}
+          completeMethod={searchApprover}
+          field='displayName'
+          onChange={(e) => {
+            if (multiple) {
+              const approvers = e.value.map(
+                ({ id, displayName, identityCard, personalNumber }) => ({
+                  id,
+                  displayName,
+                  identityCard,
+                  personalNumber,
+                })
+              );
 
-    return (
-        <div className="p-field">
-            <label htmlFor="2022"><span className="required-field">*</span>גורם מאשר</label>
-            <AutoComplete id="2022"
-                value={selectedApprover}
-                suggestions={ApproverSuggestions}
-                completeMethod={searchApprover}
-                field="displayName"
-                onChange={(e) => {
-                    const { id, displayName } = e.value
-                    setSelectedApprover(displayName)
-                    setValue(name, { id, displayName })
-                }
-                } />
-        </div>
-    )
-}
+              setSelectedApprover(approvers);
+              setValue(name, approvers);
+            }
+
+            if (!multiple) {
+              const { id, displayName, identityCard, personalNumber } = e.value;
+              setSelectedApprover(displayName);
+              setValue(name, { id, displayName, identityCard, personalNumber });
+            }
+          }}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Approver;
