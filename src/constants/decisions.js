dd44a9a52f4dd1d6c02de10fa3cf05187425ca4c
אביב בן שוהם <avivbs2468@gmail.com>
2021-10-11 10:29:40 +0300

    added decisions const

---
+++ src/constants/decisions.js
@@ -0,0 +1,13 @@
+export const DECISIONS = {
+    DECISION_UNKNOWN: 'DECISION_UNKNOWN',
+    APPROVED: 'APPROVED',
+    DENIED: 'DENIED',
+    UNRECOGNIZED: 'UNRECOGNIZED',
+};
+
+export const DECISIONS_TRANSLATE = {
+    DECISION_UNKNOWN: 'לא הוחלטה',
+    APPROVED: 'אושרה',
+    DENIED: 'נדחתה',
+    UNRECOGNIZED: 'לא ידועה',
+};
