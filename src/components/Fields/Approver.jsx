fe72d4270d955d5e425da592f7dc66c4ec1027be
liorayacob <liorayacob10@gmail.com>
2021-11-02 17:10:45 +0200

    refactor: expanded tree + toolTip for approvers field in forms.

--- src/components/Fields/Approver.jsx
+++ src/components/Fields/Approver.jsx
@@ -1,35 +1,53 @@
-import React, { useState } from 'react';
-import { searchApproverByDisplayNameReq } from '../../service/ApproverService';
-import { AutoComplete } from 'primereact/autocomplete';
-import '../../assets/css/local/components/approver.css';
+import React, { useState } from "react";
+import { searchApproverByDisplayNameReq } from "../../service/ApproverService";
+import { AutoComplete } from "primereact/autocomplete";
+import "../../assets/css/local/components/approver.css";
+import { getIn } from "yup/lib/util/reach";
 
-const Approver = ({ setValue, name, multiple, disabled, defaultApprovers, errors, trigger =null }) => {
+const Approver = ({
+  setValue,
+  name,
+  multiple,
+  disabled,
+  defaultApprovers,
+  errors,
+  trigger = null,
+}) => {
   const [ApproverSuggestions, setApproverSuggestions] = useState([]);
   const [selectedApprover, setSelectedApprover] = useState(defaultApprovers);
 
   const searchApprover = async (event) => {
     const result = await searchApproverByDisplayNameReq(
       event.query,
-      'COMMANDER'
+      "COMMANDER"
     );
     setApproverSuggestions(result.approvers);
   };
 
   return (
-    <div className='p-field-item'>
-      <div className={multiple ? 'AutoCompleteWrap' : ''}>
-        <label htmlFor='2022'>
-          <span className='required-field'>*</span>גורם מאשר
+    <div className="p-field-item">
+      <div className={multiple ? "AutoCompleteWrap" : ""}>
+        <label htmlFor="2022">
+          <span className="required-field">*</span>גורם מאשר
         </label>
         <AutoComplete
           disabled={disabled}
-          id='2022'
-          className={`approver-selection-${multiple === true ? 'multiple' : 'single'} ${disabled ? 'disabled' : ''}`}
+          id="2022"
+          className={`approver-selection-${
+            multiple === true ? "multiple" : "single"
+          } ${disabled ? "disabled" : ""}`}
           multiple={multiple}
           value={selectedApprover}
           suggestions={ApproverSuggestions}
           completeMethod={searchApprover}
-          field='displayName'
+          tooltip={selectedApprover.map(
+            (approver) => `${approver.displayName}\n`
+          )}
+          tooltipOptions={{
+            disabled: selectedApprover.length > 0 ? false : true,
+            style: { direction: "ltr" },
+          }}
+          field="displayName"
           onChange={(e) => {
             if (multiple) {
               const approvers = e.value.map(
@@ -43,7 +61,7 @@ const Approver = ({ setValue, name, multiple, disabled, defaultApprovers, errors
 
               setSelectedApprover(approvers);
               setValue(name, approvers);
-              if(trigger) trigger(name);
+              if (trigger) trigger(name);
             }
 
             if (!multiple) {
@@ -51,12 +69,17 @@ const Approver = ({ setValue, name, multiple, disabled, defaultApprovers, errors
               setSelectedApprover(displayName);
               setValue(name, { id, displayName, identityCard, personalNumber });
               if (trigger) trigger(name);
-
             }
           }}
         />
         <label htmlFor="2020">
-          {errors?.approvers && <small style={{ color: "red" }}>{errors.approvers?.message? errors.approvers?.message: "יש למלא ערך"}</small>}
+          {errors?.approvers && (
+            <small style={{ color: "red" }}>
+              {errors.approvers?.message
+                ? errors.approvers?.message
+                : "יש למלא ערך"}
+            </small>
+          )}
         </label>
       </div>
     </div>
@@ -64,7 +87,7 @@ const Approver = ({ setValue, name, multiple, disabled, defaultApprovers, errors
 };
 
 Approver.defaultProps = {
-    disabled: false
-}
+  disabled: false,
+};
 
 export default Approver;
