6b30e1f2f2cbb21059676f3a373714211ef29561
Barak Steindl <steindl.barak@gmail.com>
2021-11-29 14:09:12 +0200

    ui tiny changes

--- src/components/Modals/Role/CreateSingleRoleForm.jsx
+++ src/components/Modals/Role/CreateSingleRoleForm.jsx
@@ -1,27 +1,29 @@
-import React, { useImperativeHandle, forwardRef, useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { InputText } from "primereact/inputtext";
-import { Dropdown } from "primereact/dropdown";
-import { Button } from "primereact/button";
-import { Checkbox } from "primereact/checkbox";
-import { InputTextarea } from "primereact/inputtextarea";
-import Hierarchy from "../Hierarchy";
-import Approver from "../../Fields/Approver";
-import { useStores } from "../../../context/use-stores";
-import * as Yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { isJobTitleAlreadyTakenRequest } from "../../../service/KartoffelService";
-import {isUserHoldType} from '../../../utils/user';
+import React, { useImperativeHandle, forwardRef, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { InputText } from 'primereact/inputtext';
+import { Dropdown } from 'primereact/dropdown';
+import { Button } from 'primereact/button';
+import { Checkbox } from 'primereact/checkbox';
+import { InputTextarea } from 'primereact/inputtextarea';
+import Hierarchy from '../Hierarchy';
+import Approver from '../../Fields/Approver';
+import { useStores } from '../../../context/use-stores';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { isJobTitleAlreadyTakenRequest } from '../../../service/KartoffelService';
+import { isUserHoldType } from '../../../utils/user';
 import { USER_SOURCE_DI, USER_TYPE } from '../../../constants';
-import {GetDefaultApprovers} from '../../../utils/approver';
+import { GetDefaultApprovers } from '../../../utils/approver';
 
 // TODO: move to different file (restructe project files...)
 const validationSchema = Yup.object().shape({
   hierarchy: Yup.object().required(),
   isUserApprover: Yup.boolean(),
-  approvers: Yup.array().when("isUserApprover", {
+  approvers: Yup.array().when('isUserApprover', {
     is: false,
-    then: Yup.array().min(1, "יש לבחור לפחות גורם מאשר אחד").required("יש לבחור לפחות גורם מאשר אחד"),
+    then: Yup.array()
+      .min(1, 'יש לבחור לפחות גורם מאשר אחד')
+      .required('יש לבחור לפחות גורם מאשר אחד'),
   }),
   comments: Yup.string().optional(),
   clearance: Yup.string().required(),
@@ -34,60 +36,63 @@ const validationSchema = Yup.object().shape({
     .required(),
 });
 
-const RenameSingleOGForm = forwardRef(({ setIsActionDone, onlyForView, requestObject }, ref) => {
-  const { appliesStore, userStore } = useStores();
-  const isUserApprover = isUserHoldType(userStore.user, USER_TYPE.COMMANDER);
+const RenameSingleOGForm = forwardRef(
+  ({ setIsActionDone, onlyForView, requestObject }, ref) => {
+    const { appliesStore, userStore } = useStores();
+    const isUserApprover = isUserHoldType(userStore.user, USER_TYPE.COMMANDER);
 
-  const { register, handleSubmit, setValue, watch, formState } = useForm({
-    resolver: yupResolver(validationSchema),
-    defaultValues: { isUserApprover },
-  });
-  const { errors } = formState;
+    const { register, handleSubmit, setValue, watch, formState } = useForm({
+      resolver: yupResolver(validationSchema),
+      defaultValues: { isUserApprover },
+    });
+    const { errors } = formState;
 
-  useEffect(() => {
-    if (requestObject) {
-      setValue("comments", requestObject.comments);
-      setValue("clearance", requestObject.kartoffelParams.clearance);
-      setValue("roleName", requestObject.kartoffelParams.jobTitle);
-      setValue("hierarchy", { name: requestObject.adParams.ouDisplayName });
-      setValue("isTafkidan", !!requestObject.kartoffelParams.roleEntityType);
-    }
-  }, []);
+    useEffect(() => {
+      if (requestObject) {
+        setValue('comments', requestObject.comments);
+        setValue('clearance', requestObject.kartoffelParams.clearance);
+        setValue('roleName', requestObject.kartoffelParams.jobTitle);
+        setValue('hierarchy', { name: requestObject.adParams.ouDisplayName });
+        setValue('isTafkidan', !!requestObject.kartoffelParams.roleEntityType);
+      }
+    }, []);
 
-
-  const onSubmit = async (data) => {
-    try {
-      await validationSchema.validate(data);
-    } catch (err) {
-      throw new Error(err.errors);
-    }
-    const {
-      approvers,
-      hierarchy,
-      comments,
-      clearance,
-      roleName,
-      isTafkidan,
-    } = data;
-    const req = {
-      commanders: approvers,
-      kartoffelParams: {
-        jobTitle: roleName,
-        directGroup: hierarchy.id,
-        isRoleAttachable: true,
-        source: USER_SOURCE_DI,
-        type: "domainUser",
+    const onSubmit = async (data) => {
+      try {
+        await validationSchema.validate(data);
+      } catch (err) {
+        throw new Error(err.errors);
+      }
+      const {
+        approvers,
+        hierarchy,
+        comments,
         clearance,
-        roleEntityType: isTafkidan ? "goalUser" : undefined,
-      },
-      adParams: {
-        ouDisplayName: hierarchy.name,
-        jobTitle: roleName,
-        samAccountName: "???", // TODO: check
-      },
-      comments,
-      due: Date.now(),
-    };
+        roleName,
+        isTafkidan,
+      } = data;
+      const req = {
+        commanders: approvers,
+        kartoffelParams: {
+          jobTitle: roleName,
+          directGroup: hierarchy.id,
+          isRoleAttachable: true,
+          source: USER_SOURCE_DI,
+          type: 'domainUser',
+          clearance,
+          roleEntityType: isTafkidan ? 'goalUser' : undefined,
+        },
+        adParams: {
+          ouDisplayName: `${hierarchy.hierarchy.substring(
+            hierarchy.hierarchy.indexOf('/') + 1,
+            hierarchy.hierarchy.length
+          )}/${hierarchy.name}`,
+          jobTitle: roleName,
+          samAccountName: '???', // TODO: check
+        },
+        comments,
+        due: Date.now(),
+      };
       await appliesStore.createRoleApply(req);
       setIsActionDone(true);
     };
@@ -103,118 +108,131 @@ const RenameSingleOGForm = forwardRef(({ setIsActionDone, onlyForView, requestOb
     const onRoleNameChange = async (e) => {
       const roleNameToSearch = e.target.value;
 
-      if (roleNameToSearch && watch("hierarchy")?.id) {
+      if (roleNameToSearch && watch('hierarchy')?.id) {
         const isJobTitleAlreadyTakenResponse =
           await isJobTitleAlreadyTakenRequest(
             roleNameToSearch,
-            watch("hierarchy").id
+            watch('hierarchy').id
           );
 
-        setValue("isJobAlreadyTakenData", isJobTitleAlreadyTakenResponse);
+        setValue('isJobAlreadyTakenData', isJobTitleAlreadyTakenResponse);
       }
     };
 
     const onAvailableRoleName = (e) => {
-      setValue("roleName", e.target.innerHTML);
-      setValue("isJobAlreadyTakenData", {
+      setValue('roleName', e.target.innerHTML);
+      setValue('isJobAlreadyTakenData', {
         isJobTitleAlreadyTaken: false,
       });
     };
 
-  return (
-    <div className="p-fluid">
-      <div className="display-flex title-wrap" style={{ width: "inherit" }}>
-        <h2>היררכיה</h2>
-      </div>
-      <div className="p-fluid-item p-fluid-item-flex1">
-        <div className="p-field">
-          <Hierarchy
-            setValue={setValue}
-            name="hierarchy"
-            errors={errors}
-            ogValue={watch("hierarchy")}
-            disabled={onlyForView}
-          />
+    return (
+      <div className="p-fluid">
+        <div className="display-flex title-wrap" style={{ width: 'inherit' }}>
+          <h2>היררכיה</h2>
         </div>
-      </div>
-      <div className="p-fluid-item p-fluid-item">
-        <div className="p-field">
-          <label>
-            <span className="required-field">*</span>שם תפקיד
-          </label>
-          <span className="p-input-icon-left">
-            <i>{watch("isJobAlreadyTakenData")?.isJobTitleAlreadyTaken ? "תפוס" : "פנוי"}</i>
-            <InputText {...register("roleName")} onChange={onRoleNameChange} disabled={onlyForView} />
-            <label>{errors.roleName && <small style={{ color: "red" }}>יש למלא ערך</small>}</label>
-            <label>{errors.isJobAlreadyTakenData && <small>יש לבחור תפקיד פנוי</small>}</label>
-          </span>
-        </div>
-      </div>
-      {watch("isJobAlreadyTakenData")?.isJobTitleAlreadyTaken && (
-        <div className="p-fluid-item p-fluid-item-flex1" style={{ alignItems: "baseline", whiteSpace: "pre-wrap" }}>
-          <div className="p-field" style={{ display: "flex" }}>
-            <div style={{ marginTop: "35px" }}>שמות פנויים:</div>
-            <div style={{ margin: "20px", display: "flex", flexWrap: "wrap" }}>
-              {watch("isJobAlreadyTakenData").suggestions.map((suggestion) => (
-                <Button
-                  className="p-button-secondary p-button-outlined"
-                  style={{ width: "auto" }}
-                  onClick={onAvailableRoleName}
-                >
-                  {suggestion}
-                </Button>
-              ))}
-            </div>
+        <div className="p-fluid-item p-fluid-item-flex1">
+          <div className="p-field">
+            <Hierarchy
+              setValue={setValue}
+              name="hierarchy"
+              errors={errors}
+              ogValue={watch('hierarchy')}
+              disabled={onlyForView}
+            />
           </div>
         </div>
-      )}
-      <div className="p-fluid-item-flex p-fluid-item">
-        <div className="p-field">
-          <label>
-            <span className="required-field">*</span>סיווג תפקיד
-          </label>
-          <Dropdown
-            options={["אדום", "כחול", "סגול"]}
-            placeholder="סיווג תפקיד"
-            {...register("clearance")}
-            value={watch("clearance")}
-            disabled={onlyForView}
-          />
-          <label>{errors.clearance && <small style={{ color: "red" }}>יש למלא ערך</small>}</label>
+        <div className="p-fluid-item p-fluid-item">
+          <div className="p-field">
+            <label>
+              <span className="required-field">*</span>שם תפקיד
+            </label>
+            <span className="p-input-icon-left">
+              <i>
+                {watch('isJobAlreadyTakenData')?.isJobTitleAlreadyTaken
+                  ? 'תפוס'
+                  : 'פנוי'}
+              </i>
+              <InputText
+                {...register('roleName')}
+                onChange={onRoleNameChange}
+                disabled={onlyForView}
+              />
+              <label>
+                {errors.roleName && (
+                  <small style={{ color: 'red' }}>יש למלא ערך</small>
+                )}
+              </label>
+              <label>
+                {errors.isJobAlreadyTakenData && (
+                  <small>יש לבחור תפקיד פנוי</small>
+                )}
+              </label>
+            </span>
+          </div>
         </div>
-      </div>
-      <div className="p-fluid-item">
-        <Approver
-          setValue={setValue}
-          name="approvers"
-          tooltip='רס"ן ומעלה ביחידתך'
-          multiple={true}
-          errors={errors}
-          disabled={onlyForView || isUserApprover}
-          defaultApprovers={GetDefaultApprovers(requestObject, onlyForView)}
-        />
-      </div>
-      <div className="p-field-checkbox" style={{ marginBottom: "10px" }}>
-        <Checkbox
-          style={{ marginLeft: "10px" }}
-          {...register("isTafkidan")}
-          onChange={(e) => setValue("isTafkidan", e.checked)}
-          checked={watch("isTafkidan")}
-          disabled={onlyForView}
-        />
-        <label>התפקיד נפתח עבור משתמש תפקידן (מילואים / חמ"ל)</label>
-      </div>
-      <div className="p-fluid-item p-fluid-item-flex1">
-        <div className="p-field">
-          <label>
-            <span></span>הערות
-          </label>
-          <InputTextarea
-            {...register("comments")}
-            type="text"
-            autoResize="false"
+        {watch('isJobAlreadyTakenData')?.isJobTitleAlreadyTaken && (
+          <div
+            className="p-fluid-item p-fluid-item-flex1"
+            style={{ alignItems: 'baseline', whiteSpace: 'pre-wrap' }}
+          >
+            <div className="p-field" style={{ display: 'flex' }}>
+              <div style={{ marginTop: '35px' }}>שמות פנויים:</div>
+              <div
+                style={{ margin: '20px', display: 'flex', flexWrap: 'wrap' }}
+              >
+                {watch('isJobAlreadyTakenData').suggestions.map(
+                  (suggestion) => (
+                    <Button
+                      className="p-button-secondary p-button-outlined"
+                      style={{ width: 'auto' }}
+                      onClick={onAvailableRoleName}
+                    >
+                      {suggestion}
+                    </Button>
+                  )
+                )}
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="p-fluid-item-flex p-fluid-item">
+          <div className="p-field">
+            <label>
+              <span className="required-field">*</span>סיווג תפקיד
+            </label>
+            <Dropdown
+              options={['אדום', 'כחול', 'סגול']}
+              placeholder="סיווג תפקיד"
+              {...register('clearance')}
+              value={watch('clearance')}
+              disabled={onlyForView}
+            />
+            <label>
+              {errors.clearance && (
+                <small style={{ color: 'red' }}>יש למלא ערך</small>
+              )}
+            </label>
+          </div>
+        </div>
+        <div className="p-fluid-item">
+          <Approver
+            setValue={setValue}
+            name="approvers"
+            tooltip='רס"ן ומעלה ביחידתך'
+            multiple={true}
+            errors={errors}
+            disabled={onlyForView || isUserApprover}
+            defaultApprovers={GetDefaultApprovers(requestObject, onlyForView)}
+          />
+        </div>
+        <div className="p-field-checkbox" style={{ marginBottom: '10px' }}>
+          <Checkbox
+            style={{ marginLeft: '10px' }}
+            {...register('isTafkidan')}
+            onChange={(e) => setValue('isTafkidan', e.checked)}
+            checked={watch('isTafkidan')}
             disabled={onlyForView}
-            placeholder="הכנס הערות לבקשה..."
           />
           <label>התפקיד נפתח עבור משתמש תפקידן (מילואים / חמ"ל)</label>
         </div>
@@ -223,13 +241,36 @@ const RenameSingleOGForm = forwardRef(({ setIsActionDone, onlyForView, requestOb
             <label>
               <span></span>הערות
             </label>
-            <InputTextarea {...register("comments")} type="text" autoResize="false" disabled={onlyForView} />
-            <label>{errors.comments && <small style={{ color: "red" }}>יש למלא ערך</small>}</label>
+            <InputTextarea
+              {...register('comments')}
+              type="text"
+              autoResize="false"
+              disabled={onlyForView}
+              placeholder="הכנס הערות לבקשה..."
+            />
+            <label>התפקיד נפתח עבור משתמש תפקידן (מילואים / חמ"ל)</label>
+          </div>
+          <div className="p-fluid-item p-fluid-item-flex1">
+            <div className="p-field">
+              <label>
+                <span></span>הערות
+              </label>
+              <InputTextarea
+                {...register('comments')}
+                type="text"
+                autoResize="false"
+                disabled={onlyForView}
+              />
+              <label>
+                {errors.comments && (
+                  <small style={{ color: 'red' }}>יש למלא ערך</small>
+                )}
+              </label>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
   }
 );
 
--- src/constants/user.js
+++ src/constants/user.js
@@ -1,20 +1,20 @@
 export const USER_TYPE = {
-  SECURITY: "SECURITY",
-  SUPER_SECURITY: "SUPER_SECURITY",
-  COMMANDER: "COMMANDER",
-  SOLDIER: "SOLDIER",
-  UNRECOGNIZED: "UNRECOGNIZED",
-  ADMIN: "ADMIN",
-  BULK: "BULK",
+  SECURITY: 'SECURITY',
+  SUPER_SECURITY: 'SUPER_SECURITY',
+  COMMANDER: 'COMMANDER',
+  SOLDIER: 'SOLDIER',
+  UNRECOGNIZED: 'UNRECOGNIZED',
+  ADMIN: 'ADMIN',
+  BULK: 'BULK',
 };
 
-export const USER_CITIZEN = process.env.USER_CITIZEN || "אזרח";
-export const USER_CLEARANCE = process.env.USER_CLEARANCE || ["1", "2", "3"];
-export const USER_SOURCE_DI = process.env.USER_SOURCE_DI || "oneTree";
-export const USER_NO_PICTURE = process.env.USER_NO_PICTURE || "pictureUrl";
+export const USER_CITIZEN = process.env.USER_CITIZEN || 'אזרח';
+export const USER_CLEARANCE = process.env.USER_CLEARANCE || ['1', '2', '3'];
+export const USER_SOURCE_DI = process.env.USER_SOURCE_DI || 'oneTree';
+export const USER_NO_PICTURE = process.env.USER_NO_PICTURE || 'pictureUrl';
 
 export const USER_TYPE_TAG = {
-  APPROVER: "גורם מאשר",
-  SECURITY_APPROVER: "גורם מאשר במ",
-  ADMIN: "מחשוב יחידתי",
+  APPROVER: 'גורם מאשר',
+  SECURITY_APPROVER: 'גורם מאשר במ',
+  ADMIN: 'מחשוב יחידתי',
 };
