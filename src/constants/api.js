25049b0ccf54b0796a3fedc3aa9c0d967f5a3b26
nettashviro <netta.zhviro@gmail.com>
2022-01-09 12:53:25 +0200

    envload

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,7 @@
-// export const apiBaseUrl = process.env.REACT_APP_BASE_URL || "http://localhost:2000";
-export const apiBaseUrl =
-  window.location.hostname === 'localhost' ? 'http://localhost:2000' : '';
+import env from 'react-dotenv';
 
-export const tokenName = process.env.UI_REACT_TOKEN_NAME || 'sp-token';
-export const pageSize = process.env.UI_REACT_PAGE_SIZE || 10;
-export const itemsInPage = process.env.UI_REACT_ITEMS_IN_PAGE || 6;
-export const firstPage = process.env.UI_REACT_FIRST_PAGE || 0;
+export const apiBaseUrl = env.hostname === "localhost" ? "http://localhost:2000" : "";
+export const tokenName = env.UI_REACT_TOKEN_NAME || 'sp-token';
+export const pageSize = env.UI_REACT_PAGE_SIZE || 10;
+export const itemsInPage = env.UI_REACT_ITEMS_IN_PAGE || 6;
+export const firstPage = env.UI_REACT_FIRST_PAGE || 0;
--- src/constants/user.js
+++ src/constants/user.js
@@ -1,46 +1,19 @@
-export const USER_TYPE = {
-  SECURITY: 'SECURITY',
-  SUPER_SECURITY: 'SUPER_SECURITY',
-  COMMANDER: 'COMMANDER',
-  SOLDIER: 'SOLDIER',
-  UNRECOGNIZED: 'UNRECOGNIZED',
-  ADMIN: 'ADMIN',
-  BULK: 'BULK',
-};
+import env from "react-dotenv";
 
-export const USER_CITIZEN_ENTITY_TYPE =
-  process.env.UI_USER_CITIZEN_ENTITY_TYPE || 'agumon';
+// ENV
+export const USER_CITIZEN_ENTITY_TYPE = env.UI_USER_CITIZEN_ENTITY_TYPE || "agumon";
+export const USER_CLEARANCE = env.UI_USER_CLEARANCE? env.UI_USER_CLEARANCE.split(","): ["1", "2", "3", "4", "5", "6"];
+export const USER_SOURCE_DI = env.UI_USER_SOURCE_DI || "sf_name";
+export const USER_NO_PICTURE = env.UI_USER_NO_PICTURE_STRING || "pictureUrl";
+export const highCommanderRanks = env.UI_HIGH_COMMANDER_RANKS? env.UI_HIGH_COMMANDER_RANKS.split(","): ["rookie", "champion"];
+export const USER_DI_TYPE = env.UI_DI_TYPE || "domainUser";
+export const USER_ROLE_ENTITY_TYPE = env.UI_ROLE_ENTITY_TYPE || "goalUser";
 
-// export const USER_CITIZEN_ENTITY_TYPE =
-  // process.env.UI_USER_CITIZEN_ENTITY_TYPE || 'Civilian';
-
-export const USER_CLEARANCE = process.env.UI_USER_CLEARANCE
-  ? process.env.UI_USER_CLEARANCE.split(",")
-  : ["1", "2", "3", "4", "5", "6"];
-
-export const USER_SOURCE_DI = process.env.UI_USER_SOURCE_DI || 'sf_name';
-
-// export const USER_SOURCE_DI = process.env.UI_USER_SOURCE_DI || 'ADservices';
-export const USER_NO_PICTURE = process.env.UI_USER_NO_PICTURE_STRING || 'pictureUrl';
-
-export const highCommanderRanks = process.env.UI_HIGH_COMMANDER_RANKS
-  ? process.env.UI_HIGH_COMMANDER_RANKS.split(',')
-  : ['rookie', 'champion'];
-
-// export const highCommanderRanks = process.env.UI_HIGH_COMMANDER_RANKS
-  // ? process.env.UI_HIGH_COMMANDER_RANKS.split(",")
-  // : ["סאל", "אלם", "תאל", "אלף"];
-  
-export const USER_DI_TYPE = process.env.UI_DI_TYPE || "domainUser";
-
-export const USER_ROLE_ENTITY_TYPE = process.env.UI_ROLE_ENTITY_TYPE || "goalUser";
-// export const USER_ROLE_ENTITY_TYPE =
-  // process.env.UI_ROLE_ENTITY_TYPE || 'GoalUser';
-
+// CONST
 export const USER_TYPE_TAG = {
-  APPROVER: 'גורם מאשר',
-  SECURITY_APPROVER: 'גורם מאשר במ',
-  ADMIN: 'מחשוב יחידתי',
+  APPROVER: "גורם מאשר",
+  SECURITY_APPROVER: "גורם מאשר במ",
+  ADMIN: "מחשוב יחידתי",
 };
 
 export const USER_SEX = [
@@ -52,5 +25,15 @@ export const USER_SEX = [
 export const USER_ENTITY_TYPE = {
   Soldier: "חייל",
   Civilian: "אזרח",
-  GoalUser: "GoalUser"
-}
+  GoalUser: "GoalUser",
+};
+
+export const USER_TYPE = {
+  SECURITY: "SECURITY",
+  SUPER_SECURITY: "SUPER_SECURITY",
+  COMMANDER: "COMMANDER",
+  SOLDIER: "SOLDIER",
+  UNRECOGNIZED: "UNRECOGNIZED",
+  ADMIN: "ADMIN",
+  BULK: "BULK",
+};
