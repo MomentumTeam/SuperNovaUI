e9812fbf7399ecb4acc018f6b9deec55aab71334
nettashviro <netta.zhviro@gmail.com>
2022-01-10 11:48:15 +0200

    add logs

--- src/constants/api.js
+++ src/constants/api.js
@@ -5,3 +5,4 @@ export const tokenName = env.UI_REACT_TOKEN_NAME || 'sp-token';
 export const pageSize = env.UI_REACT_PAGE_SIZE || 10;
 export const itemsInPage = env.UI_REACT_ITEMS_IN_PAGE || 6;
 export const firstPage = env.UI_REACT_FIRST_PAGE || 0;
+console.log("check-env-api");
