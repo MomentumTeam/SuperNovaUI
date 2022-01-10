73dc9426ce9787672fc4de32f3b1172d9408c158
nettashviro <netta.zhviro@gmail.com>
2022-01-10 17:15:49 +0200

    change

--- preconfigure_server.sh
+++ preconfigure_server.sh
@@ -2,8 +2,8 @@
 
 cat .env | while read line; do
     IFS='=' read key value <<< $line
-    stringField=`"$key": ""`
+    stringField='"'$key'": ""'
     fieldValue=printenv $key
-    stringFieldNew=`"$key": $fieldValue`
+    stringFieldNew='"'$key'": "'$fieldValue'"'
     sed -i "s/${stringField}/${stringFieldNew}g" /usr/share/nginx/html/env.js
 done
