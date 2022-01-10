6aeeae2c73a9c2b7e0276f05954e7d04a9fe8c0a
nettashviro <netta.zhviro@gmail.com>
2022-01-10 13:49:03 +0200

    change

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,8 @@
 import env from 'react-dotenv';
 
 export const apiBaseUrl = env.hostname === "localhost" ? "http://localhost:2000" : "";
-export const tokenName = env.UI_REACT_TOKEN_NAME || 'sp-token';
-export const pageSize = env.UI_REACT_PAGE_SIZE || 10;
+export const tokenName = env.REACT_APP_TOKEN_NAME || 'sp-token';
+export const pageSize = env.REACT_APP_PAGE_SIZE || 10;
 export const itemsInPage = env.UI_REACT_ITEMS_IN_PAGE || 6;
 export const firstPage = env.UI_REACT_FIRST_PAGE || 0;
-console.log("check-env-api");
+console.log("check-env-api", process.env.REACT_APP_TOKEN_NAME);
