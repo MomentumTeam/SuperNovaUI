04790c11b0d7ef892c77f5d8c297d598d2de7ea9
liorayacob <liorayacob10@gmail.com>
2022-01-20 12:13:17 +0200

    refactor: ContactInfo - added SECUIRTY+SUPER_SECUIRT contact Info

--- src/store/Config.js
+++ src/store/Config.js
@@ -3,19 +3,21 @@ import { getConfig } from '../service/ConfigService';
 
 class ConfigStore {
   // USER
-  USER_CITIZEN_ENTITY_TYPE = "digimon";
-  USER_CLEARANCE = ["1", "2", "3", "4", "5", "6"];
-  USER_SOURCE_DI = "sf_name";
-  USER_NO_PICTURE = "pictureUrl";
-  USER_HIGH_COMMANDER_RANKS = ["rookie", "champion"];
-  USER_DI_TYPE = "domainUser";
-  USER_ROLE_ENTITY_TYPE = "goalUser";
+  USER_CITIZEN_ENTITY_TYPE = 'digimon';
+  USER_CLEARANCE = ['1', '2', '3', '4', '5', '6'];
+  USER_SOURCE_DI = 'sf_name';
+  USER_NO_PICTURE = 'pictureUrl';
+  USER_HIGH_COMMANDER_RANKS = ['rookie', 'champion'];
+  USER_DI_TYPE = 'domainUser';
+  USER_ROLE_ENTITY_TYPE = 'goalUser';
 
   // GENERAL
-  TOKEN_NAME = "sp-token";
+  TOKEN_NAME = 'sp-token';
   PAGE_SIZE = 10;
   ITEMS_IN_PAGE = 6;
   FIRST_PAGE = 0;
+  SECUIRTY_MAIL = 'T82130201@gmail.com';
+  SUPER_SECUIRTY_MAIL = 'T02250B49@gmail.com';
 
   constructor() {
     makeAutoObservable(this, {
@@ -30,24 +32,33 @@ class ConfigStore {
       PAGE_SIZE: observable,
       ITEMS_IN_PAGE: observable,
       FIRST_PAGE: observable,
+      SECUIRTY_MAIL: observable,
+      SUPER_SECUIRTY_MAIL: observable,
     });
   }
 
   async loadConfig() {
     try {
       const config = await getConfig();
-      if (config?.USER_CITIZEN_ENTITY_TYPE) this.USER_CITIZEN_ENTITY_TYPE = config.USER_CITIZEN_ENTITY_TYPE;
+      if (config?.USER_CITIZEN_ENTITY_TYPE)
+        this.USER_CITIZEN_ENTITY_TYPE = config.USER_CITIZEN_ENTITY_TYPE;
       if (config?.USER_CLEARANCE) this.USER_CLEARANCE = config.USER_CLEARANCE;
       if (config?.USER_SOURCE_DI) this.USER_SOURCE_DI = config.USER_SOURCE_DI;
-      if (config?.USER_NO_PICTURE) this.USER_NO_PICTURE = config.USER_NO_PICTURE;
-      if (config?.USER_HIGH_COMMANDER_RANKS) this.USER_HIGH_COMMANDER_RANKS = config.USER_HIGH_COMMANDER_RANKS;
+      if (config?.USER_NO_PICTURE)
+        this.USER_NO_PICTURE = config.USER_NO_PICTURE;
+      if (config?.USER_HIGH_COMMANDER_RANKS)
+        this.USER_HIGH_COMMANDER_RANKS = config.USER_HIGH_COMMANDER_RANKS;
       if (config?.USER_DI_TYPE) this.USER_DI_TYPE = config.USER_DI_TYPE;
-      if (config?.USER_ROLE_ENTITY_TYPE) this.USER_ROLE_ENTITY_TYPE = config.USER_ROLE_ENTITY_TYPE;
+      if (config?.USER_ROLE_ENTITY_TYPE)
+        this.USER_ROLE_ENTITY_TYPE = config.USER_ROLE_ENTITY_TYPE;
       if (config?.TOKEN_NAME) this.TOKEN_NAME = config.TOKEN_NAME;
       if (config?.PAGE_SIZE) this.PAGE_SIZE = config.PAGE_SIZE;
       if (config?.FIRST_PAGE) this.FIRST_PAGE = config.FIRST_PAGE;
+      if (config?.SECUIRTY_MAIL) this.SECUIRTY_MAIL = config.SECUIRTY_MAIL;
+      if (config?.SUPER_SECUIRTY_MAIL)
+        this.SUPER_SECUIRTY_MAIL = config.SUPER_SECUIRTY_MAIL;
     } catch (error) {
-      console.log("problem with config");
+      console.log('problem with config');
     }
   }
 }
