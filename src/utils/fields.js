2f21592175a2b80a352562420474013f7a143abb
nettashviro <netta.zhviro@gmail.com>
2022-02-02 13:37:19 +0200

    fix

--- src/utils/fields.js
+++ src/utils/fields.js
@@ -1,8 +1,14 @@
 import configStore from '../store/Config';
 
+export const getUserRelevantIdentity = (user) => {
+  return user?.digitalIdentities && Array.isArray(user.digitalIdentities) ? user.digitalIdentities.find(
+    (identity) => identity.source === configStore.USER_SOURCE_DI
+  ): [];
+};
+
 export const getSamAccountNameFromEntity = (entity) => {
-  const di = entity.digitalIdentities.find((di) => di.source === configStore.USER_SOURCE_DI);
-  return di?.uniqueId ? di.uniqueId.split("@")[0]: '';
+  const di = getUserRelevantIdentity(entity);
+  return di?.uniqueId ? di.uniqueId.split("@")[0] : "";
 };
 
 export const getSamAccountNameFromUniqueId = (uniqueId) => {
